40fdcc03e4f9d45200a55c0978853747f8c374ec
Galdineris <30872149+Galdineris@users.noreply.github.com>
2017-11-07 20:58:46 -0200

    Add files via upload

--- Galdineris/Blocagem.py
+++ Galdineris/Blocagem.py
@@ -4,14 +4,20 @@ import sys
 
 pygame.init()
 
-screen = pygame.display.set_mode((600,400))
+screen = pygame.display.set_mode((600,700))
 
 pygame.display.set_caption("Hercules")
 
 screen.fill((0,255,0))
 
-pygame
-pygame.display.flip()
+green = (255,255,255)
+
+posicao = [250,200]
+
+tora = pygame.image.load("tora.png")
+imagerect = tora.get_rect()
+
+pygame.display.update()
 
 while True:
     for event in pygame.event.get():
@@ -23,10 +29,16 @@ while True:
                 pygame.quit()
                 sys.exit()
             if event.key == pygame.K_LEFT:
+
+                screen.blit(tora, imagerect)
                 print("esquerda")
+                pygame.display.update()
                 
             if event.key == pygame.K_RIGHT:
+                
                 print("direita")
+                pygame.display.update()
+
     pygame.display.flip()
 
 
