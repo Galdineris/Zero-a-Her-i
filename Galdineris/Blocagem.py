c7ff5103dc588545b894098211989bb466b2e443
Galdineris <30872149+Galdineris@users.noreply.github.com>
2017-11-09 19:55:49 -0200

    Add files via upload

--- Galdineris/Blocagem.py
+++ Galdineris/Blocagem.py
@@ -10,13 +10,14 @@ pygame.display.set_caption("Hercules")
 
 screen.fill((0,255,0))
 
-green = (255,255,255)
+white = (255,255,255)
 
 posicao = [250,200]
 
-tora = pygame.image.load("tora.png")
-imagerect = tora.get_rect()
-
+tora = pygame.Rect(200,400,200,100)
+##image.load("tora.png")
+##imagerect = tora.get_rect()
+pygame.draw.rect(screen,white,tora,3)
 pygame.display.update()
 
 while True:
@@ -28,16 +29,35 @@ while True:
             if event.key == pygame.K_ESCAPE:
                 pygame.quit()
                 sys.exit()
+                
             if event.key == pygame.K_LEFT:
-
-                screen.blit(tora, imagerect)
+                tora.left -= 10
+                screen.fill((0,255,0))
+                pygame.draw.rect(screen,white,tora,3)
                 print("esquerda")
                 pygame.display.update()
-                
             if event.key == pygame.K_RIGHT:
-                
+                tora.right += 10
+                screen.fill((0,255,0))
+                pygame.draw.rect(screen,white,tora,3)
                 print("direita")
                 pygame.display.update()
+
+                
+##            if event.key == pygame.K_UP:
+##                tora.top -= 10
+##                screen.fill((0,255,0))
+##                pygame.draw.rect(screen,white,tora,3)
+##                print("direita")
+##                pygame.display.update()
+##                
+##            if event.key == pygame.K_DOWN:
+##                tora.top += 10
+##                screen.fill((0,255,0))
+##                pygame.draw.rect(screen,white,tora,3)
+##                print("direita")
+##                pygame.display.update()
+
 
     pygame.display.flip()
 
