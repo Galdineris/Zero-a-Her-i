43c5bc5745df5c8645fb25d1fdc35e4196108e03
Galdineris <30872149+Galdineris@users.noreply.github.com>
2017-11-10 21:21:52 -0200

    Add files via upload

--- Galdineris/Blocagem.py
+++ Galdineris/Blocagem.py
@@ -1,6 +1,16 @@
 #26/10
 import pygame
+import time
 import sys
+
+def draw_tela():
+    screen.fill((0,255,0))
+    pygame.draw.rect(screen,white,hidra1,3)
+    pygame.draw.rect(screen,white,hidra2,3)
+    pygame.draw.rect(screen,white,hidra3,3)
+    pygame.draw.rect(screen,white,hidra4,3)
+    pygame.display.update()
+
 
 pygame.init()
 
@@ -14,17 +24,26 @@ white = (255,255,255)
 
 posicao = [250,200]
 
-hidra = pygame.Rect(200,550,200,100)
+hidra1 = pygame.Rect(200,550,200,100)
+hidra2 = pygame.Rect(200,450,200,100)
+hidra3 = pygame.Rect(200,350,200,100)
+hidra4 = pygame.Rect(200,250,200,100)
 hercules1 = pygame.Rect(140,550,50,100)
 hercules2 = pygame.Rect(410,550,50,100)
 
 ##image.load("hidra.png")
 ##imagerect = hidra.get_rect()
-pygame.draw.rect(screen,white,hidra,3)
+pygame.draw.rect(screen,white,hidra1,3)
+pygame.draw.rect(screen,white,hidra2,3)
+pygame.draw.rect(screen,white,hidra3,3)
+pygame.draw.rect(screen,white,hidra4,3)
 pygame.draw.rect(screen, white,hercules1, 3 )
+
+
 pygame.display.update()
 
 while True:
+    
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
@@ -33,21 +52,27 @@ while True:
             if event.key == pygame.K_ESCAPE:
                 pygame.quit()
                 sys.exit()
-                
             if event.key == pygame.K_LEFT:
-                hidra.top -= 10
                 screen.fill((0,255,0))
                 pygame.draw.rect(screen, white,hercules1,3)
+                for i in range(100):
+                    time.sleep(0.00000002)
+                    hidra1.top += 1
+                    pygame.draw.rect(screen, white,hercules1,3)
+                    draw_tela()
                 print("esquerda")
                 
             if event.key == pygame.K_RIGHT:
-                hidra.top += 10
-                screen.fill((0,255,0))
+                for i in range(100):
+                    time.sleep(0.00000002)
+                    hidra1.top -= 1
+                    pygame.draw.rect(screen,white,hidra1,3)
+                    draw_tela()
+                    
                 pygame.draw.rect(screen, white,hercules2,3)
                 print("direita")
 
-        pygame.draw.rect(screen, white,hidra,3)
-        pygame.display.update()
+        draw_tela()
 
             
 ##            if event.key == pygame.K_UP:
